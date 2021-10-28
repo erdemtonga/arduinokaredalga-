22b53bb440b660882b9a0cfa264eec4c74a3769b
Erdem Tonga <47635473+erdemtonga@users.noreply.github.com>
2021-10-28 14:49:00 +0300

    Add files via upload

---
+++ osilaskop2.ino
@@ -0,0 +1,11 @@
+double y, y_eski, x, a = 0.01;
+void setup() {
+Serial.begin(115200);
+}
+void loop() {
+y_eski = y;
+x = analogRead(A0) * (5.0 / 1023.0);
+y = a * x + (1 - a) * y_eski;
+Serial.println(y);
+delay(1); // delay in between reads for stability
+}
